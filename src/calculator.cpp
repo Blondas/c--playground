d50fcfd8015f28d67e44a182f99578734e7874dd
Krzysiek Nowakowski <knowakowski@pm.me>
2024-11-27 17:57:56 +0100

    feat: add .gitignore file to exclude unnecessary files from version control feat(calculator): implement Calculator class with add and subtract methods feat(hello-world): create a simple Hello World program in C++

---
+++ src/calculator.cpp
@@ -0,0 +1,9 @@
+#include "../include/calculator.h"
+
+double Calculator::add(double a, double b) {
+    return a + b;
+}
+
+double Calculator::subtract(double a, double b) {
+    return a - b;
+}
---
+++ src/hello-world.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout << "Hello Worldaa" << std::endl;
+    return 0;
+}
